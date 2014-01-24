b286807b696a7e18c2158edca0d222561a001be9
Dylan Kuhn <dylan.k.kuhn@gmail.com>
2014-01-23 21:30:29 -0800

    Try an absolute path for uploads.

--- include/NoteFactory.php
+++ include/NoteFactory.php
@@ -4,7 +4,7 @@ require_once('Database.php');
 require_once('Note.php');
 require_once(dirname(__FILE__).'/pel/PelJpeg.php');
 
-define ('IMAGE_PATH', '../uploads/');
+define ('IMAGE_PATH', dirname( dirname( __FILE__ ) ) . '/uploads/' );
 
 class NoteFactory
 {
