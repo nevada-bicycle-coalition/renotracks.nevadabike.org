bf8e9e0455253f26e247672ed2f417adf34fb3cc
Dylan Kuhn <dylan.k.kuhn@gmail.com>
2014-02-27 15:48:13 -0800

    Cache trip data but not the list.

--- www/routes/routeData.php
+++ www/routes/routeData.php
@@ -2,12 +2,13 @@
 include_once('CoordFactory.php');
 include_once('TripFactory.php');
 
-$max_age = 60*60*24;
-header( "Cache-Control: public,max-age=$max_age" );
-header( 'Expires: ' . gmdate( 'D, d M Y H:i:s', time() + $max_age ) . ' GMT' );
-header( 'Content-Type: application/json' );
 
 if($_GET['t']=="get_coords_by_trip"){
+	$max_age = 60*60*24;
+	header( "Cache-Control: public,max-age=$max_age" );
+	header( 'Expires: ' . gmdate( 'D, d M Y H:i:s', time() + $max_age ) . ' GMT' );
+	header( 'Content-Type: application/json' );
+
 	$obj = new CoordFactory();
 	$coords = $obj->getCoordsByTrip($_GET['q']);
 	header( 'Content-Length: '. strlen( $coords ) );
